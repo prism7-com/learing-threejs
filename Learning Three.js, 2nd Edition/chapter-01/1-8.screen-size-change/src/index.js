4daa1a7dc09c2319dfe9e476c8eae2a3a90a670c
prism7games <85687503+prism7games@users.noreply.github.com>
2021-08-24 00:28:35 +0900

    1-8 completed

--- Learning Three.js, 2nd Edition/chapter-01/1-8.screen-size-change/src/index.js
+++ Learning Three.js, 2nd Edition/chapter-01/1-8.screen-size-change/src/index.js
@@ -122,3 +122,11 @@ function initStats() {
 
   return stats;
 }
+
+function onResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onResize, false);
