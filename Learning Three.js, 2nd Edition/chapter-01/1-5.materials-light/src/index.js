54c5406154a98c74c9a1cf7a3a3e19dbf947dfee
prism7games <85687503+prism7games@users.noreply.github.com>
2021-08-23 22:14:14 +0900

    first commit

---
+++ Learning Three.js, 2nd Edition/chapter-01/1-5.materials-light/src/index.js
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+import { ShapeGeometry, Sphere } from "three";
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(new THREE.Color(0xeeeeee));
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 座標軸
+var axes = new THREE.AxesHelper(20);
+scene.add(axes);
+
+// plane
+var planeGeometry = new THREE.PlaneGeometry(60, 20);
+var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+plane.position.x = 15;
+plane.position.y = 0;
+plane.position.z = 0;
+scene.add(plane);
+
+// cube
+var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+var cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cube.position.x = -4;
+cube.position.y = 3;
+cube.position.z = 0;
+scene.add(cube);
+
+// sphere
+var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+var sphereMaterial = new THREE.MeshBasicMaterial({
+  color: 0x7777ff,
+  wireframe: true,
+});
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.x = 20;
+sphere.position.y = 4;
+sphere.position.z = 2;
+scene.add(sphere);
+
+camera.position.x = -30;
+camera.position.y = 40;
+camera.position.z = 30;
+camera.lookAt(scene.position);
+
+document.getElementById("WebGL-output").appendChild(renderer.domElement);
+renderer.render(scene, camera);
