a4bd8e0ebad7dc9954e20a68207b429645083214
prism7games <85687503+prism7games@users.noreply.github.com>
2021-08-28 16:13:13 +0900

    2-3-1 complete

--- Learning Three.js, 2nd Edition/chapter-02/2-3-1.both-cameras/src/index.js
+++ Learning Three.js, 2nd Edition/chapter-02/2-3-1.both-cameras/src/index.js
@@ -9,11 +9,11 @@ const stats = initStats();
 const scene = new THREE.Scene();
 
 // カメラの作成
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = -30;
-camera.position.y = 40;
-camera.position.z = 30;
-camera.lookAt(new THREE.Vector3(5, 0, 0));
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.x = 120;
+camera.position.y = 60;
+camera.position.z = 180;
+camera.lookAt(scene.position);
 scene.add(camera);
 
 // レンダラ
@@ -23,106 +23,65 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 
 // 床面
-const planeGeometry = new THREE.PlaneGeometry(60, 40, 1, 1);
+const planeGeometry = new THREE.PlaneGeometry(180, 180);
 const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.receiveShadow = true;
 plane.rotation.x = -0.5 * Math.PI;
 plane.position.x = 0;
 plane.position.y = 0;
 plane.position.z = 0;
 scene.add(plane);
 
-// ambient light
-var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-scene.add(ambientLight);
+// directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+directionalLight.position.set(-20, 40, 60);
+scene.add(directionalLight);
 
-// spotlight
-const spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(-20, 30, 10);
-spotLight.castShadow = true;
-scene.add(spotLight);
+// ambient light
+var ambientLight = new THREE.AmbientLight(0x292929);
+scene.add(ambientLight);
 
 document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
 // Cube
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+for (let j = 0; j < planeGeometry.parameters.height / 5; j++) {
+  for (let i = 0; i < planeGeometry.parameters.width / 5; i++) {
+    const rnd = Math.random() * 0.75 + 0.25;
+    const cubeMatelial = new THREE.MeshLambertMaterial({ color: new THREE.Color(rnd, 0, 0) });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMatelial);
+    cube.position.z = -(planeGeometry.parameters.height / 2) + 2 + j * 5;
+    cube.position.x = -(planeGeometry.parameters.width / 2) + 2 + i * 5;
+    cube.position.y = 2;
+    scene.add(cube);
+  }
+}
+
+// GUI
 const controls = new (function () {
-  this.scaleX = 1;
-  this.scaleY = 1;
-  this.scaleZ = 1;
-
-  this.positionX = 0;
-  this.positionY = 0;
-  this.positionZ = 0;
-
-  this.rotationX = 0;
-  this.rotationY = 0;
-  this.rotationZ = 0;
-
-  this.scale = 1;
-
-  this.translateX = 0;
-  this.translateY = 0;
-  this.translateZ = 0;
-
-  this.visible = true;
-
-  this.translate = function () {
-    cube.translateX(controls.translateX);
-    cube.translateY(controls.translateY);
-    cube.translateZ(controls.translateZ);
-
-    this.positionX = cube.position.x;
-    this.positionY = cube.position.y;
-    this.positionZ = cube.position.z;
+  this.perspective = "Perspective";
+  this.switchCamera = function () {
+    if (camera instanceof THREE.PerspectiveCamera) {
+      camera = new THREE.OrthographicCamera(window.innerWidth / -16, window.innerWidth / 16, window.innerHeight / 16, window.innerHeight / -16, -200, 500);
+      camera.position.x = 120;
+      camera.position.y = 60;
+      camera.position.z = 180;
+      camera.lookAt(scene.position);
+      this.perspective = "Orthographic";
+    } else {
+      camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera.position.x = 120;
+      camera.position.y = 60;
+      camera.position.z = 180;
+      camera.lookAt(scene.position);
+      this.perspective = "Perspective";
+    }
   };
 })();
 
-const matelial = new THREE.MeshLambertMaterial({ color: 0x44ff44 });
-const geom = new THREE.BoxGeometry(5, 8, 3);
-const cube = new THREE.Mesh(geom, matelial);
-cube.position.y = 4;
-cube.castShadow = true;
-scene.add(cube);
-
-// GUI
 const gui = new dat.GUI();
-
-const guiScale = gui.addFolder("scale");
-guiScale.add(controls, "scaleX", 0, 5);
-guiScale.add(controls, "scaleY", 0, 5);
-guiScale.add(controls, "scaleZ", 0, 5);
-
-const guiPosition = gui.addFolder("potision");
-const contX = guiPosition.add(controls, "positionX", -10, 10);
-const contY = guiPosition.add(controls, "positionY", -4, 20);
-const contZ = guiPosition.add(controls, "positionZ", -10, 10);
-
-contX.listen();
-contX.onChange(function () {
-  cube.position.x = controls.positionX;
-});
-contY.listen();
-contY.onChange(function () {
-  cube.position.y = controls.positionY;
-});
-contZ.listen();
-contZ.onChange(function () {
-  cube.position.z = controls.positionZ;
-});
-
-const guiRotation = gui.addFolder("rotation");
-guiRotation.add(controls, "rotationX", -4, 4);
-guiRotation.add(controls, "rotationY", -4, 4);
-guiRotation.add(controls, "rotationZ", -4, 4);
-
-const guiTranslate = gui.addFolder("translate");
-guiTranslate.add(controls, "translateX", -10, 10);
-guiTranslate.add(controls, "translateY", -10, 10);
-guiTranslate.add(controls, "translateZ", -10, 10);
-guiTranslate.add(controls, "translate");
-
-gui.add(controls, "visible");
+gui.add(controls, "switchCamera");
+gui.add(controls, "perspective").listen();
 
 // レンダリング
 renderScene();
@@ -132,13 +91,6 @@ renderScene();
  */
 function renderScene() {
   stats.update();
-
-  cube.visible = controls.visible;
-  cube.rotation.x = controls.rotationX;
-  cube.rotation.y = controls.rotationY;
-  cube.rotation.z = controls.rotationZ;
-
-  cube.scale.set(controls.scaleX, controls.scaleY, controls.scaleZ);
 
   // requestAnimationFrameを利用してレンダリング（レンダリングタイミングをブラウザに任せる）
   requestAnimationFrame(renderScene);
