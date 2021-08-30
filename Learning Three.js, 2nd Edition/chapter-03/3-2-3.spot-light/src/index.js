a42e75cbdb7d426eac18b45e30ad3a3c96e927d3
prism7games <85687503+prism7games@users.noreply.github.com>
2021-08-31 02:47:39 +0900

    3-2-3 complete

--- Learning Three.js, 2nd Edition/chapter-03/3-2-3.spot-light/src/index.js
+++ Learning Three.js, 2nd Edition/chapter-03/3-2-3.spot-light/src/index.js
@@ -22,27 +22,11 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(new THREE.Color(0xeeeeee));
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
-// ambient light
-let ambiColor = "#0c0c0c";
-const ambientLight = new THREE.AmbientLight(ambiColor);
-scene.add(ambientLight);
-
-// spot light
-// const spotLight = new THREE.SpotLight(0xffffff);
-// spotLight.position.set(-40, 60, -10);
-// spotLight.castShadow = true;
-// scene.add(spotLight);
-
-// point light
-let pointColor = "#ccffcc";
-const pointLight = new THREE.PointLight(pointColor);
-pointLight.distance = 100;
-scene.add(pointLight);
-
 // 床面
-const planeGeometry = new THREE.PlaneGeometry(60, 20, 20, 20);
+const planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
 const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
@@ -54,7 +38,7 @@ scene.add(plane);
 
 // Cube
 const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-const cubeMatelial = new THREE.MeshLambertMaterial({ color: 0xff7777 });
+const cubeMatelial = new THREE.MeshLambertMaterial({ color: 0xff3333 });
 const cube = new THREE.Mesh(cubeGeometry, cubeMatelial);
 cube.castShadow = true;
 cube.position.x = -4;
@@ -72,45 +56,111 @@ sphere.position.y = 0;
 sphere.position.z = 2;
 scene.add(sphere);
 
+// ambient light
+let ambiColor = "#1c1c1c";
+const ambientLight = new THREE.AmbientLight(ambiColor);
+scene.add(ambientLight);
+
+// spot light0
+const spotLight0 = new THREE.SpotLight(0xcccccc);
+spotLight0.position.set(-40, 30, -10);
+spotLight0.lookAt(plane.position);
+scene.add(spotLight0);
+
+// spot light
+let spotLightColor = "#ffffff";
+const spotLight = new THREE.SpotLight(spotLightColor);
+spotLight.position.set(-40, 60, -10);
+spotLight.castShadow = true;
+spotLight.shadow.camera.near = 2;
+spotLight.shadow.camera.far = 200;
+spotLight.shadow.camera.pov = 30;
+spotLight.target = plane;
+spotLight.decay = 1;
+spotLight.distance = 0;
+spotLight.angle = 0.4;
+scene.add(spotLight);
+
 // Sphere Light
 const sphereLightGeometry = new THREE.SphereGeometry(0.2);
 const sphereLightMaterial = new THREE.MeshBasicMaterial({ color: 0xac6c25 });
 const sphereLight = new THREE.Mesh(sphereLightGeometry, sphereLightMaterial);
 sphereLight.castShadow = true;
-sphereLight.position.set(new THREE.Vector3(3, 0, 3));
+sphereLight.position.set(new THREE.Vector3(3, 20, 3));
 scene.add(sphereLight);
+
+// Camera Helper
+const cameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+cameraHelper.visible = false;
+scene.add(cameraHelper);
 
 // GUI
 const controls = new (function () {
   this.rotationSpeed = 0.02;
   this.bouncingSpeed = 0.03;
   this.ambientColor = ambiColor;
-  this.pointColor = pointColor;
+  this.spotLightColor = spotLightColor;
   this.intensity = 1;
-  this.distance = 100;
   this.decay = 1;
+  this.distance = 0;
+  this.penumbra = 30;
+  this.angle = 0.1;
+  this.debug = false;
+  this.castShadow = true;
+  this.target = "Plane";
+  this.stopMovingLight = false;
 })();
 
 const gui = new dat.GUI();
 gui.addColor(controls, "ambientColor").onChange(function (e) {
   ambientLight.color = new THREE.Color(e);
 });
-gui.addColor(controls, "pointColor").onChange(function (e) {
-  pointLight.color = new THREE.Color(e);
+gui.addColor(controls, "spotLightColor").onChange(function (e) {
+  spotLight.color = new THREE.Color(e);
 });
-gui.add(controls, "intensity", 0, 3).onChange(function (e) {
-  pointLight.intensity = e;
+gui.add(controls, "angle", 0, Math.PI * 2).onChange(function (e) {
+  spotLight.angle = e;
 });
-gui.add(controls, "distance", 0, 100).onChange(function (e) {
-  pointLight.distance = e;
+gui.add(controls, "intensity", 0, 5).onChange(function (e) {
+  spotLight.intensity = e;
 });
 gui.add(controls, "decay", 1, 100).onChange(function (e) {
-  pointLight.decay = e;
+  spotLight.decay = e;
+});
+gui.add(controls, "distance", 0, 200).onChange(function (e) {
+  spotLight.distance = e;
+});
+gui.add(controls, "penumbra", 0, 100).onChange(function (e) {
+  spotLight.penumbra = e;
+});
+gui.add(controls, "debug").onChange(function (e) {
+  cameraHelper.visible = e;
+});
+gui.add(controls, "castShadow").onChange(function (e) {
+  spotLight.castShadow = e;
+});
+gui.add(controls, "target", ["Plane", "Sphere", "Cube"]).onChange(function (e) {
+  console.log(e);
+  switch (e) {
+    case "Plane":
+      spotLight.target = plane;
+      break;
+    case "Sphere":
+      spotLight.target = sphere;
+      break;
+    case "Cube":
+      spotLight.target = cube;
+      break;
+  }
+});
+gui.add(controls, "stopMovingLight").onChange(function (e) {
+  stopMovingLight = e;
 });
 
 // レンダリング
 renderScene();
 
+let stopMovingLight = false;
 let step = 0;
 let invert = 1;
 let phase = 0;
@@ -132,22 +182,24 @@ function renderScene() {
   sphere.position.y = 2 + 10 * Math.abs(Math.sin(step));
 
   // light animation(simulation)
-  if (phase > 2 * Math.PI) {
-    invert = invert * -1;
-    phase -= 2 * Math.PI;
-  } else {
-    phase += controls.rotationSpeed;
-  }
-  sphereLight.position.z = 7 * Math.sin(phase);
-  sphereLight.position.x = 14 * Math.cos(phase);
-  sphereLight.position.y = 5;
+  if (!stopMovingLight) {
+    if (phase > 2 * Math.PI) {
+      invert = invert * -1;
+      phase -= 2 * Math.PI;
+    } else {
+      phase += controls.rotationSpeed;
+    }
+    sphereLight.position.z = 7 * Math.sin(phase);
+    sphereLight.position.x = 14 * Math.cos(phase);
+    sphereLight.position.y = 10;
 
-  if (invert < 0) {
-    const pivot = 14;
-    sphereLight.position.x = invert * (sphereLight.position.x - pivot) + pivot;
-  }
+    if (invert < 0) {
+      const pivot = 14;
+      sphereLight.position.x = invert * (sphereLight.position.x - pivot) + pivot;
+    }
 
-  pointLight.position.copy(sphereLight.position);
+    spotLight.position.copy(sphereLight.position);
+  }
 
   // requestAnimationFrameを利用してレンダリング（レンダリングタイミングをブラウザに任せる）
   requestAnimationFrame(renderScene);
