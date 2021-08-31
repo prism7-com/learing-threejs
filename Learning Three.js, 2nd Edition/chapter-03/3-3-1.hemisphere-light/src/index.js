849bf96d9c5aab84c961388ad8b0130012fa7dbf
prism7games <85687503+prism7games@users.noreply.github.com>
2021-08-31 13:43:42 +0900

    3-3-1 complete

--- Learning Three.js, 2nd Edition/chapter-03/3-3-1.hemisphere-light/src/index.js
+++ Learning Three.js, 2nd Edition/chapter-03/3-3-1.hemisphere-light/src/index.js
@@ -1,19 +1,20 @@
 import * as THREE from "three";
 import Stats from "stats.js";
 import * as dat from "dat.gui";
-import { Sphere } from "three";
+import image from "../../../assets/textures/ground/grasslight-big.jpg";
 
 // 統計情報の追加
 const stats = initStats();
 
 // シーンの作成
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0xaaaaaa, 0.01, 200);
 
 // カメラの作成
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = -35;
-camera.position.y = 30;
-camera.position.z = 25;
+camera.position.x = -20;
+camera.position.y = 15;
+camera.position.z = 45;
 camera.lookAt(new THREE.Vector3(10, 0, 0));
 scene.add(camera);
 
@@ -22,17 +23,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(new THREE.Color(0xeeeeee));
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
 document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
+// 床面のテクスチャ
+const textureLoader = new THREE.TextureLoader();
+const textureGrass = textureLoader.load(image);
+textureGrass.wrapS = THREE.RepeatWrapping;
+textureGrass.wrapT = THREE.RepeatWrapping;
+textureGrass.repeat.set(4, 4);
 
 // 床面
 const planeGeometry = new THREE.PlaneGeometry(600, 200, 20, 20);
-const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+const planeMaterial = new THREE.MeshLambertMaterial({ map: textureGrass });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
 plane.rotation.x = -0.5 * Math.PI;
 plane.position.x = 15;
-plane.position.y = -5;
+plane.position.y = 0;
 plane.position.z = 0;
 scene.add(plane);
 
@@ -47,13 +54,13 @@ cube.position.z = 0;
 scene.add(cube);
 
 // Sphere
-const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+const sphereGeometry = new THREE.SphereGeometry(4, 25, 25);
 const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.castShadow = true;
-sphere.position.x = 20;
-sphere.position.y = 0;
-sphere.position.z = 2;
+sphere.position.x = 10;
+sphere.position.y = 5;
+sphere.position.z = 10;
 scene.add(sphere);
 
 // ambient light
@@ -61,84 +68,59 @@ let ambiColor = "#1c1c1c";
 const ambientLight = new THREE.AmbientLight(ambiColor);
 scene.add(ambientLight);
 
+// spot light
+const spotLight = new THREE.SpotLight(0xcccccc);
+spotLight.position.set(-40, 60, -10);
+spotLight.lookAt(plane);
+scene.add(spotLight);
+
+// hemi light
+const hemiLight = new THREE.HemisphereLight(0x0000ff, 0x00ff00, 0.6);
+hemiLight.position.set(0, 500, 0);
+scene.add(hemiLight);
+
 // directional light
-let directionalLightColor = "#ff5808";
+let directionalLightColor = "#ffffff";
 const directionalLight = new THREE.DirectionalLight(directionalLightColor);
-directionalLight.position.set(-40, 60, -10);
+directionalLight.position.set(30, 10, -50);
 directionalLight.castShadow = true;
-directionalLight.shadow.camera.near = 2;
+directionalLight.target = plane;
+directionalLight.shadow.camera.near = 0.1;
 directionalLight.shadow.camera.far = 200;
 directionalLight.shadow.camera.left = -50;
 directionalLight.shadow.camera.right = 50;
 directionalLight.shadow.camera.top = 50;
 directionalLight.shadow.camera.bottom = -50;
-directionalLight.distance = 0;
-directionalLight.intensity = 0.5;
-directionalLight.target = plane;
 directionalLight.shadow.mapSize.height = 1024;
 directionalLight.shadow.mapSize.width = 1024;
 scene.add(directionalLight);
-
-// Sphere Light
-const sphereLightGeometry = new THREE.SphereGeometry(0.2);
-const sphereLightMaterial = new THREE.MeshBasicMaterial({ color: 0xac6c25 });
-const sphereLight = new THREE.Mesh(sphereLightGeometry, sphereLightMaterial);
-sphereLight.castShadow = true;
-sphereLight.position.set(new THREE.Vector3(3, 20, 3));
-scene.add(sphereLight);
-
-// Camera Helper
-const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-cameraHelper.visible = false;
-scene.add(cameraHelper);
 
 // GUI
 const controls = new (function () {
   this.rotationSpeed = 0.02;
   this.bouncingSpeed = 0.03;
-  this.ambientColor = ambiColor;
-  this.directionalLightColor = directionalLightColor;
+  this.hemisphere = true;
+  this.color = 0x00ff00;
+  this.skyColor = 0x0000ff;
   this.intensity = 0.5;
-  this.distance = 0;
-  this.penumbra = 30;
-  this.angle = 0.1;
-  this.debug = false;
-  this.castShadow = true;
-  this.target = "Plane";
 })();
 
 const gui = new dat.GUI();
-gui.addColor(controls, "ambientColor").onChange(function (e) {
-  ambientLight.color = new THREE.Color(e);
+gui.add(controls, "hemisphere").onChange(function (e) {
+  if (!e) {
+    hemiLight.intensity = 0;
+  } else {
+    hemiLight.intensity = controls.intensity;
+  }
 });
-gui.addColor(controls, "directionalLightColor").onChange(function (e) {
-  directionalLight.color = new THREE.Color(e);
+gui.addColor(controls, "color").onChange(function (e) {
+  hemiLight.groundColor = new THREE.Color(e);
+});
+gui.addColor(controls, "skyColor").onChange(function (e) {
+  hemiLight.color = new THREE.Color(e);
 });
 gui.add(controls, "intensity", 0, 5).onChange(function (e) {
   directionalLight.intensity = e;
-});
-gui.add(controls, "distance", 0, 200).onChange(function (e) {
-  directionalLight.distance = e;
-});
-gui.add(controls, "debug").onChange(function (e) {
-  cameraHelper.visible = e;
-});
-gui.add(controls, "castShadow").onChange(function (e) {
-  directionalLight.castShadow = e;
-});
-gui.add(controls, "target", ["Plane", "Sphere", "Cube"]).onChange(function (e) {
-  console.log(e);
-  switch (e) {
-    case "Plane":
-      directionalLight.target = plane;
-      break;
-    case "Sphere":
-      directionalLight.target = sphere;
-      break;
-    case "Cube":
-      directionalLight.target = cube;
-      break;
-  }
 });
 
 // レンダリング
@@ -161,12 +143,6 @@ function renderScene() {
   step += controls.bouncingSpeed;
   sphere.position.x = 20 + 10 * Math.cos(step);
   sphere.position.y = 2 + 10 * Math.abs(Math.sin(step));
-
-  // light animation(simulation)
-  sphereLight.position.z = -8;
-  sphereLight.position.y = 27 * Math.sin(step / 3);
-  sphereLight.position.x = 10 + 26 * Math.cos(step / 3);
-  directionalLight.position.copy(sphereLight.position);
 
   // requestAnimationFrameを利用してレンダリング（レンダリングタイミングをブラウザに任せる）
   requestAnimationFrame(renderScene);
