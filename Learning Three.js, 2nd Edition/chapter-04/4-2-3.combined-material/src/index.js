12f67331b67b25219b2b93608e6b2ce52d34b579
prism7games <85687503+prism7games@users.noreply.github.com>
2021-09-01 22:48:27 +0900

    first commit.

---
+++ Learning Three.js, 2nd Edition/chapter-04/4-2-3.combined-material/src/index.js
@@ -0,0 +1,129 @@
+import * as THREE from "three";
+import Stats from "stats.js";
+import * as dat from "dat.gui";
+
+// 統計情報の追加
+const stats = initStats();
+
+// [Scene]シーンの作成
+const scene = new THREE.Scene();
+scene.overrideMaterial = new THREE.MeshDepthMaterial();
+
+// [Camera]カメラの作成
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 30, 170);
+camera.position.x = -50;
+camera.position.y = 40;
+camera.position.z = 50;
+camera.lookAt(scene.position);
+scene.add(camera);
+
+// [Renderer]レンダラ
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(new THREE.Color(0xeeeeee));
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
+// [Gui]GUI
+const controls = new (function () {
+  this.cubeCount = scene.children.length - 1;
+  this.cameraNear = camera.near;
+  this.cameraFar = camera.far;
+  this.rotationSpeed = 0.02;
+  this.numberOfObjects = scene.children.length;
+
+  this.removeCube = function () {
+    const allChildren = scene.children;
+    const lastObject = allChildren[allChildren.length - 1];
+    if (lastObject instanceof THREE.Mesh) {
+      scene.remove(lastObject);
+      this.numberOfObjects = scene.children.length;
+      this.cubeCount = scene.children.length - 1;
+    }
+  };
+  this.addCube = function () {
+    const cubeSize = Math.ceil(3 + Math.random() * 3);
+    const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMaterial = new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff });
+    // const cubeMaterial = new THREE.MeshDepthMaterial();
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
+    cube.position.x = -60 + Math.round(Math.random() * 100);
+    cube.position.y = Math.round(Math.random() * 10);
+    cube.position.z = -100 + Math.round(Math.random() * 150);
+    cube.name = "Cube" + scene.children.length;
+    scene.add(cube);
+    this.numberOfObjects = scene.children.length;
+    this.cubeCount = scene.children.length - 1;
+  };
+})();
+
+const gui = new dat.GUI();
+gui.add(controls, "cubeCount").listen();
+gui.add(controls, "rotationSpeed", 0, 0.5);
+gui.add(controls, "addCube");
+gui.add(controls, "removeCube");
+gui.add(controls, "cameraNear", 0, 50).onChange(function (e) {
+  camera.near = e;
+  camera.updateProjectionMatrix();
+});
+gui.add(controls, "cameraFar", 100, 300).onChange(function (e) {
+  camera.far = e;
+  camera.updateProjectionMatrix();
+});
+
+// キューブ初期配置
+let i = 0;
+while (i < 10) {
+  console.log("Cube", i);
+  controls.addCube();
+  i++;
+}
+
+// レンダリング
+renderScene();
+
+/**
+ * シーンのレンダリング（アニメーション）
+ */
+function renderScene() {
+  stats.update();
+
+  // mesh animation
+  scene.traverse(function (e) {
+    if (e instanceof THREE.Mesh) {
+      e.rotation.x += controls.rotationSpeed;
+      e.rotation.y += controls.rotationSpeed;
+      e.rotation.z += controls.rotationSpeed;
+    }
+  });
+
+  // requestAnimationFrameを利用してレンダリング（レンダリングタイミングをブラウザに任せる）
+  requestAnimationFrame(renderScene);
+  renderer.render(scene, camera);
+}
+
+/**
+ * 統計情報の初期化
+ * @returns 初期化済みの統計情報コンポーネント
+ */
+function initStats() {
+  var stats = new Stats();
+  stats.setMode(0);
+
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.left = "0px";
+  stats.domElement.style.top = "0px";
+
+  document.getElementById("Stats-output").appendChild(stats.domElement);
+
+  return stats;
+}
+
+function onResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onResize, false);
