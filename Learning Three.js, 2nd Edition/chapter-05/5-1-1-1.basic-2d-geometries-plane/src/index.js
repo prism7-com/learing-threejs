01e73d65520e75918cda3e5b211139ba7c2cb461
prism7games <85687503+prism7games@users.noreply.github.com>
2021-09-12 23:56:40 +0900

    5-1-1-1 complete

--- Learning Three.js, 2nd Edition/chapter-05/5-1-1-1.basic-2d-geometries-plane/src/index.js
+++ Learning Three.js, 2nd Edition/chapter-05/5-1-1-1.basic-2d-geometries-plane/src/index.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Stats from "stats.js";
-import gosper from "./gosper";
+import * as dat from "dat.gui";
+import { SceneUtils } from "three/examples/jsm/utils/SceneUtils";
 
 // 統計情報の追加
 const stats = initStats();
@@ -18,14 +19,10 @@ scene.add(camera);
 
 // [Renderer]レンダラ
 const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(new THREE.Color(0x000000));
+renderer.setClearColor(new THREE.Color(0xeeeeee));
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 document.getElementById("WebGL-output").appendChild(renderer.domElement);
-
-// [Light] ambient light
-var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-scene.add(ambientLight);
 
 // [Light]spot light
 const spotLight = new THREE.SpotLight(0xffffff);
@@ -33,36 +30,48 @@ spotLight.position.set(-40, 60, -10);
 spotLight.castShadow = true;
 scene.add(spotLight);
 
-// [Line] mesh
-const points = gosper(4, 60);
-const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-const lineMaterial = new THREE.LineDashedMaterial({
-  vertexColors: true,
-  color: 0xffffff,
-  dashSize: 2,
-  gapSize: 2,
-  scale: 0.1,
-});
+// [Mesh] plane
+let plane = createMesh(new THREE.PlaneGeometry(10, 14, 4, 4));
+scene.add(plane);
 
-const colors = [];
-let i = 0;
-points.forEach(function (e) {
-  const color = new THREE.Color(0xffffff);
-  color.setHSL(e.x / 100 + 0.5, (e.y * 20) / 300, 0.8);
-  colors.push(color.r, color.g, color.b);
-  i++;
-});
-lineGeometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+// [Gui]Gui
+const controls = new (function () {
+  this.width = plane.children[0].geometry.parameters.width;
+  this.height = plane.children[0].geometry.parameters.width;
 
-const line = new THREE.Line(lineGeometry, lineMaterial);
-line.computeLineDistances();
-line.position.set(40, -70, -30);
-scene.add(line);
+  this.widthSegments = plane.children[0].geometry.parameters.widthSegments;
+  this.heightSegments = plane.children[0].geometry.parameters.heightSegments;
+
+  this.redraw = function () {
+    scene.remove(plane);
+    plane = createMesh(new THREE.PlaneGeometry(controls.width, controls.height, Math.round(controls.widthSegments), Math.round(controls.heightSegments)));
+    scene.add(plane);
+  };
+})();
+
+const gui = new dat.GUI();
+gui.add(controls, "width", 0, 40).onChange(controls.redraw);
+gui.add(controls, "height", 0, 40).onChange(controls.redraw);
+gui.add(controls, "widthSegments", 1, 10, 1).onChange(controls.redraw);
+gui.add(controls, "heightSegments", 1, 10, 1).onChange(controls.redraw);
 
 let step = 0;
 
 // レンダリング
 renderScene();
+
+/**
+ * メッシュの作成
+ * @param {Geometry} geometry
+ */
+function createMesh(geometry) {
+  const meshMaterial = new THREE.MeshNormalMaterial();
+  meshMaterial.side = THREE.DoubleSide;
+  const wireFrameMaterial = new THREE.MeshBasicMaterial();
+  wireFrameMaterial.wireframe = true;
+  const plane = SceneUtils.createMultiMaterialObject(geometry, [meshMaterial, wireFrameMaterial]);
+  return plane;
+}
 
 /**
  * シーンのレンダリング（アニメーション）
@@ -72,7 +81,7 @@ function renderScene() {
 
   // mesh animation
   step += 0.01;
-  line.rotation.y = step;
+  plane.rotation.y = step;
 
   // requestAnimationFrameを利用してレンダリング（レンダリングタイミングをブラウザに任せる）
   requestAnimationFrame(renderScene);
@@ -84,7 +93,7 @@ function renderScene() {
  * @returns 初期化済みの統計情報コンポーネント
  */
 function initStats() {
-  var stats = new Stats();
+  const stats = new Stats();
   stats.setMode(0);
 
   stats.domElement.style.position = "absolute";
